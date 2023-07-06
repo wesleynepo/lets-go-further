0c1b52b3cfadea963418d65d30cf48b1d50f931a
wesleynepo <wesleynepomuceno1@gmail.com>
2023-07-05 22:26:03 -0300

    finished

---
+++ Makefile
@@ -0,0 +1,51 @@
+include .envrc 
+
+.PHONY: help
+help:
+	@echo 'Usage:'
+	@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' | sed -e 's/
+
+.PHONY: confirm 
+confirm:
+	@echo -n 'Are you sure? [y/N] ' && read ans && [ $${ans:-N} = y ]
+
+## run/api: run the cmd/api application
+.PHONY: run/api 
+run/api:
+	go run ./cmd/api -db-dsn=${GREENLIGHT_DB_DSN}
+
+## db/psql: connect to the database using psql 
+.PHONY: db/psql
+db/psql:
+	psql ${GREENLIGHT_DB_DSN}
+
+## db/migrations/new name=$1: create a new database migration
+.PHONY: db/migrations/new 
+db/migrations/new:
+	@echo 'Running up migrations...'
+	migrate -path ./migrations -database ${GREENLIGHT_DB_DSN} up
+
+## db/migrations/up: apply all up database migrations
+.PHONY: db/migrations/up
+db/migrations/up: confirm
+	@echo 'Creating migration files for ${name}...'
+	migrate create -seq -ext=.sql -dir=./migrations ${name}
+
+
+.PHONY: audit
+audit:
+	@echo 'Tidying and verifying module dependencies...'
+	go mod tidy
+	go mod verify
+	@echo 'Formatting code...'
+	go fmt ./...
+	@echo 'Vetting code...'
+	go vet ./...
+	staticcheck ./...
+	@echo 'Running tests...'
+	go test -race -vet=off ./...
+
+.PHONY: build/api
+build/api:
+	@echo 'Building cmd/api...'
+	go build -ldflags='-s -X main.buildTime=${current_time}' -o=./bin/api ./cmd/api
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,52 +1,148 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"expvar"
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
+	"sync"
 	"time"
+
+	_ "github.com/lib/pq"
+	"greenlight.wesley/internal/data"
+	"greenlight.wesley/internal/jsonlog"
+	"greenlight.wesley/internal/mailer"
 )
-
-
 
 const version = "1.0.0"
 
+var buildTime string
+
 type config struct {
-    port int
-    env string
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
-    config config 
-    logger *log.Logger
+	config config
+	logger *jsonlog.Logger
+	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
-    var cfg config
+	var cfg config
 
-    flag.IntVar(&cfg.port, "port", 4000, "API server port")
-    flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-    flag.Parse()
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 
-    logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 
-    app := &application{config: cfg, logger: logger}
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
-    mux := http.NewServeMux()
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "727d90cb9b7693", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "53270540ec7ac3", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+        displayVersion := flag.Bool("version", false, "Display version and exit")
 
-    mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+        flag.Parse()
 
-    srv := &http.Server{
-        Addr: fmt.Sprint(":%d", cfg.port),
-        Handler: mux,
-        IdleTimeout: time.Minute,
-        ReadTimeout: 10 * time.Second,
-        WriteTimeout: 30 * time.Second,
-    }
+        if *displayVersion {
+            fmt.Printf("Version:\t%s\n", version)
+            fmt.Printf("Build time:\t%s\n", buildTime)
+            os.Exit(0)
+        }
 
-    logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-    err := srv.ListenAndServe()
-    logger.Fatal(err)
+
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+
+
+
+	defer db.Close()
+
+	logger.PrintInfo("database connection pool estabilished", nil)
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("database", expvar.Func(func() interface{} {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() interface{} {
+		return time.Now().Unix()
+	}))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+
+	logger.PrintFatal(err, nil)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
